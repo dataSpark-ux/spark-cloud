cc823d7652bdedc4c0484b59d3fb6fb0f9077823
wy <837466309@qq.com>
2021-08-10 11:22:23 +0800

    first commit

--- spark-gateway/src/main/java/com/spark/gateway/filter/AuthFilter.java
+++ spark-gateway/src/main/java/com/spark/gateway/filter/AuthFilter.java
@@ -1,22 +1,19 @@
 package com.spark.gateway.filter;
 
 import cn.hutool.json.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
+
 import java.util.List;
 
 /**
@@ -25,6 +22,7 @@ import java.util.List;
  * @createTime 2021/08/09
  */
 @Configuration
+@Slf4j
 public class AuthFilter implements GlobalFilter {
 
 
