3b507a745c0114ef2df171b35cc9e09c1b71cf53
wy <837466309@qq.com>
2021-08-11 15:50:28 +0800

    集成xxl_job

---
+++ spark-job/spark-job-executor/src/main/java/com/spark/job/executor/XxlJobExecutorApplication.java
@@ -0,0 +1,16 @@
+package com.spark.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyi
+ */
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+    }
+
+}
